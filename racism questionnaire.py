3dc2580b67862abd15cf7595c0010935d030bae9
Funeralsafari <156730255+Funeralsafari@users.noreply.github.com>
2024-01-15 20:57:20 -0500

    Add files via upload

---
+++ racism questionnaire.py
@@ -0,0 +1,10 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/lovin/AppData/Local/Programs/Python/Python312/Racism 2.py
+We recommend this test be taken privately to ensure personal reflection and honesty.
+For each question, please respond with a number from 1 to 5, where:
+1 = Strongly Disagree, 2 = Disagree, 3 = Neutral, 4 = Agree, 5 = Strongly Agree.
+Please answer honestly based on your feelings and beliefs.
+
+How comfortable do you feel in multicultural environments?
